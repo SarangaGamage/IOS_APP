8c17e29d4c0dc18512aa0d322717b6e90316eba9
saranga24 <sarangag@emojot.com>
2023-09-23 09:48:20 +0530

    Initial Commit

---
+++ IOS_CW1/IOS_CW1App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_CW1App.swift
+//  IOS_CW1
+//
+//  Created by Saranga Gamage on 2023-09-23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_CW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
