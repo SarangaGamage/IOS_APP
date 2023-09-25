cb511220842d1c06eef7ba1b7a0702cb44082461
SarangaGamage <sarangag@emojot.com>
2023-09-25 22:44:57 +0530

    income add and savings add complete

---
+++ IOS_CW1/UICommon/IncomeAdd.swift
@@ -0,0 +1,118 @@
+//
+//  IncomeAdd.swift
+//  IOS_CW1
+//
+//  Created by Saranga Gamage on 2023-09-25.
+//
+
+import SwiftUI
+
+struct IncomeAdd: View {
+    @Binding var isPresented: Bool
+    @State private var amount = ""
+    @State private var messageText = ""
+    @State private var isCategoryAdded = false
+    @State private var showAlert = false
+    @EnvironmentObject var userSessionManager: UserSessionManager
+
+    var body: some View {
+        VStack {
+            Text("Add Income")
+                .font(.title)
+                .padding()
+            
+            InputTextField(text: $amount, placeholder: "Add Your Income Amount")
+
+            HStack {
+                Button(action: {
+
+                    let email = userSessionManager.userEmail
+                    guard let amountDouble = Double(amount) else {
+                        return
+                    }
+                    
+                    let categoryData: [String: Any] = [
+                        "email": "saranga@gmail.com",
+                        "amount": amountDouble
+                    ]
+                    
+
+                    guard let jsonData = try? JSONSerialization.data(withJSONObject: categoryData) else {
+
+                        return
+                    }
+
+                    guard let url = URL(string: "http://localhost:3000/addIncome") else {
+
+                        return
+                    }
+
+                    var request = URLRequest(url: url)
+                    request.httpMethod = "POST"
+                    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                    request.httpBody = jsonData
+
+
+                    URLSession.shared.dataTask(with: request) { data, response, error in
+                        if let error = error {
+                            print("Error: \(error)")
+                        } else if let data = data {
+                            if let responseJSON = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                                if let success = responseJSON["success"] as? Bool, let message = responseJSON["message"] as? String {
+                                    DispatchQueue.main.async {
+                                        messageText = message
+                                        showAlert = true
+                                        isCategoryAdded = success
+                                    }
+                                }
+                            }
+                        }
+                    }.resume()
+                }) {
+                    Text("Add")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color(#colorLiteral(red: 0.1960784314, green: 0.1450980392, blue: 0.5568627451, alpha: 1)))
+                        .cornerRadius(50)
+                }
+                .padding()
+
+                Spacer()
+
+                Button(action: {
+                    isPresented = false
+                }) {
+                    Text("Cancel")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundColor(Color("PrimaryColor"))
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .cornerRadius(50.0)
+                        .shadow(color: Color.black.opacity(0.08), radius: 10, x: 0.0, y: 0.0)
+                }
+                .padding()
+            }
+            .padding(.horizontal, -15)
+        }
+        .background(Color.white)
+        .cornerRadius(16)
+        .padding()
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Message"),
+                message: Text(messageText),
+                dismissButton: .default(Text("OK")) {
+                    if isCategoryAdded {
+                        isPresented = false
+                    }
+                }
+            )
+        }
+    }
+}
+
--- IOS_CW1/View/Dashboard/DashboardScreenview.swift
+++ IOS_CW1/View/Dashboard/DashboardScreenview.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
-import SwiftPieChart
+
 
 struct DashboardView: View {
     @State private var income: Double = 5000
     @State private var expenses: Double = 3000
     @State private var savings: Double = 2000
+    @State private var isIncomeAddPresented = false
+    @State private var isSavingAddPresented = false
 
     var body: some View {
         ScrollView {
@@ -30,14 +32,12 @@ struct DashboardView: View {
                         .frame(width: 400, height: 450)
                         .cornerRadius(25)
 
-
                     ZStack {
                         ArcShape()
                             .foregroundColor(Color(#colorLiteral(red: 0.937254902, green: 0.537254902, blue: 0.062745098, alpha: 1)))
                         ArcShape(start: 0, end: 230)
                             .foregroundColor(Color(#colorLiteral(red: 0.1960784314, green: 0.1450980392, blue: 0.5568627451, alpha: 1)))
                             .shadow(color: Color.secondary.opacity(0.5), radius: 7)
-
                     }
                     .frame(width: UIScreen.main.bounds.width * 0.72, height: UIScreen.main.bounds.width * 0.72)
                     .padding(.bottom, 18)
@@ -60,33 +60,27 @@ struct DashboardView: View {
 
                     VStack {
                         HStack {
-                            StatusButton(title: "Income", value: "12", color: Color(#colorLiteral(red: 0.4392156863, green: 1, blue: 0.4941176471, alpha: 1))){}
+                            StatusButton(title: "Income", value: "12", color: Color(#colorLiteral(red: 0.4392156863, green: 1, blue: 0.4941176471, alpha: 1))) {
+                                isIncomeAddPresented = true
+                            }
 
-                            StatusButton(title: "Expenses", value: "$19.99", color: Color(#colorLiteral(red: 1.0, green: 0.6274509804, blue: 0.6274509804, alpha: 1.0)))
- {}
+                            StatusButton(title: "Expenses", value: "$19.99", color: Color(#colorLiteral(red: 1.0, green: 0.6274509804, blue: 0.6274509804, alpha: 1.0))) {
+                             
+                            }
 
-                            StatusButton(title: "Savings", value: "$5.99", color: Color(#colorLiteral(red: 0.502, green: 0.549, blue: 1.0, alpha: 1.0)))
- {}
+                            StatusButton(title: "Savings", value: "$5.99", color: Color(#colorLiteral(red: 0.502, green: 0.549, blue: 1.0, alpha: 1.0))) {
+                                isSavingAddPresented = true
+                            }
                         }
                         .padding(.top, 310)
                     }
                     .padding(.horizontal, 20)
                 }
 
-//                VStack {
-//                    PieChartView(
-//                        values: [1300, 500, 300],
-//                        names: ["Rent", "Transport", "Education"],
-//                        formatter: { value in String(format: "$%.2f", value) },
-//                        colors: [Color.red, Color.purple, Color.orange],
-//                        backgroundColor: Color.white)
-//                        .frame(height: 200)
-//                        .padding(.top, 20)
-//                        .padding(.horizontal)
-//                }
-
                 VStack {
-                    FinancialComponentView(title: "Income", amount: income, color: .green)
+
+                        FinancialComponentView(title: "Income", amount: income, color: .green)
+  
                     FinancialComponentView(title: "Expenses", amount: expenses, color: .red)
                     FinancialComponentView(title: "Savings", amount: savings, color: .blue)
                 }
@@ -95,8 +89,15 @@ struct DashboardView: View {
             }
         }
         .edgesIgnoringSafeArea(.top)
+        .sheet(isPresented: $isIncomeAddPresented) {
+            IncomeAdd(isPresented: $isIncomeAddPresented)
+        }
+        .sheet(isPresented: $isSavingAddPresented) {
+            SavingAdd(isPresented: $isSavingAddPresented)
+        }
     }
 }
+
 
 
 struct FinancialComponentView: View {
